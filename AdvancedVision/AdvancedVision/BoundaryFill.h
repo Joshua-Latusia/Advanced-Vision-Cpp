02e4423baff8f4fc0d2c60f014db9eb837f49280
Joshua Latusia <jcdslatu@avans.nl>
2018-09-26 15:49:10 +0200

    Renamed "Close"boundary stuff to "Double" + added generating of double boundary

--- AdvancedVision/AdvancedVision/BoundaryFill.h
+++ AdvancedVision/AdvancedVision/BoundaryFill.h
@@ -15,8 +15,17 @@ public:
 	/// <param name="contourVec">The contour vec.</param>
 	/// <param name="regionPixels">The region pixels.</param>
 	/// <returns></returns>
-	static int getEnclosedPixels(const cv::Mat& image, const std::vector<cv::Point>& contourVec, std::vector<cv::Point>& regionPixels);
+	static int getEnclosedPixels(const cv::Mat& image, const std::vector<cv::Point>& boundaryVec, std::vector<cv::Point>& regionPixels);
 	
+	/// <summary>
+	/// Generates the extra boundary around the regular boundary so BoundaryFill algorithm will work 8-connected.
+	/// </summary>
+	/// <param name="image">The boundary image image with the boundaries having a value &gt; 0.</param>
+	/// <param name="closeBoundaryImage">The boundary image with extra border with the value of 101.</param>
+	/// <param name="boundaryVec">The boundary vector of the image. In the original order of the boundary path</param>
+	/// <param name="doubleBoundaryVec">The double boundary vector, also contains the points that should be a double border but already are a single border.</param>
+	static void generateDoubleBoundary(const cv::Mat& image, cv::Mat& doubleBoundaryImage, const std::vector<cv::Point>& boundaryVec, std::vector<cv::Point>& doubleBoundaryVec);
+
 	/// <summary>
 	/// Gets the filled image by filling up the contour image
 	/// </summary>
