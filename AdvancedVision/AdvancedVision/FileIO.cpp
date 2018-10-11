a19583340d9ca628dc9991c8cfdd04b7e5f2b290
Joshua Latusia <jcdslatu@avans.nl>
2018-10-11 12:54:54 +0200

    Add getting filenames from a dir, with possible filter

--- AdvancedVision/AdvancedVision/FileIO.cpp
+++ AdvancedVision/AdvancedVision/FileIO.cpp
@@ -1,6 +1,9 @@
 #include "FileIO.h"
 #include <opencv2/imgcodecs/imgcodecs_c.h>
 #include <opencv2/imgcodecs.hpp>
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem;
 
 
 FileIO::FileIO()
@@ -20,4 +23,33 @@ bool FileIO::saveImage(const cv::Mat& image, const std::string& path = "", const
 	// Check if file can be read after saving
 	cv::Mat img = cv::imread(path, CV_LOAD_IMAGE_COLOR);
 	return img.data;
+}
+
+void FileIO::getFilesFromDir(const std::string path, std::vector<std::string>& filenames, const std::vector<std::string> extensionFilter)
+{
+	// get all files in dir
+	for (auto& s : fs::directory_iterator(path))
+	{
+		filenames.push_back(s.path().string());
+	}
+
+	// filter by extension
+	if (!extensionFilter.empty())
+	{
+		filterByExtension(filenames, extensionFilter);
+	}
+}
+
+void FileIO::filterByExtension(std::vector<std::string>& filenames, const std::vector<std::string> extensionFilter)
+{
+	std::vector<std::string> result;
+	for (auto& filename : filenames)
+	{
+		fs::path filePath = filename;
+		if (std::find(extensionFilter.begin(), extensionFilter.end(), filePath.extension()) != extensionFilter.end())
+		{
+			result.push_back(filename);
+		}
+	}
+	filenames = result;
 }
