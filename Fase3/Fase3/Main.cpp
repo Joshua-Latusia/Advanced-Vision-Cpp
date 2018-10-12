0b0672f89dfc6e65d71e589ec30647658bb5ab1e
Unknown <ctmeijen@avans.nl>
2018-10-12 11:22:31 +0200

    i dont even know anymore

--- Fase3/Fase3/Main.cpp
+++ Fase3/Fase3/Main.cpp
@@ -35,24 +35,68 @@ int main(int argc, char* argv[])
 {
 	Main main;
 	//main.setTestData("Key", 0);
-	//main.setTestData("Hearts", (cv::Mat_<double>(1, 2) << 0 , 0));
-	//main.setTestData("Schoppen", (cv::Mat_<double>(1, 2) << 1, 0));
+	main.setTestData("Hearts", (cv::Mat_<double>(1, 1) << 0 ));
+	//main.setTestData("Circles", (cv::Mat_<double>(1, 2) << 1, 0));
 	//main.setTestData("Squares", (cv::Mat_<double>(1, 2) << 0, 1));
-	//main.setTestData("Lightningbolt", (cv::Mat_<double>(1, 2) << 1, 1));
+	main.setTestData("Lightningbolt", (cv::Mat_<double>(1, 1) << 1));
 
 	std::cout << "Training Input " << std::endl << std::endl;
 	std::cout << ITset << std::endl << std::endl;
 	std::cout << "Training Output " << std::endl << std::endl;
 	std::cout << OTset << std::endl << std::endl;
 
-	//main.trainNetwork();
+	main.trainNetwork();
 	//main.TestLightning();
 	main.runCamera();
 	getchar();
 }
 
 
+void Main::CheckContour(std::vector<cv::Point> contourPoints)
+{
+	//calculates circularity
+	double perimeter = cv::arcLength(contourPoints, true);
+	double area = cv::contourArea(contourPoints);
+	double circularity = 4 * CV_PI *(area / (perimeter*perimeter));
+	std::vector<cv::Point> approx;
 
+	//calculate if contour is convex
+
+
+	std::vector<cv::Point> convexHull;
+	cv::convexHull(contourPoints, convexHull);
+	bool isConvex = cv::isContourConvex(convexHull);
+
+	std::vector<int> convexHullI;
+	cv::convexHull(contourPoints, convexHullI);
+	std::vector<cv::Vec4i> defectPoints(convexHullI.size());
+	cv::convexityDefects(contourPoints, convexHullI, defectPoints);
+
+	auto hull_area = cv::contourArea(convexHull);
+
+	// calculate solidity
+	auto solidity = area / hull_area;
+
+	cv::Mat	IVec = (cv::Mat_<double>(1, 3) <<
+		circularity, (double)defectPoints.size() / 100, solidity);
+
+	std::cout << "circularity: " << circularity << std::endl;
+	std::cout << "isCOnvex: " << isConvex << std::endl;
+	std::cout << "convex defects: " << (double)defectPoints.size() / 100 << std::endl;
+	std::cout << "solidity: " << solidity << std::endl;
+
+
+
+
+
+	auto outputVectorBPN = BPN(IVec, V0, W0);
+
+	// druk de output vector van het BPN af in dezelfde regel afgesloten met |
+	for (int r = 0; r < outputVectorBPN.rows; r++)
+		std::cout << std::setw(8) << round(getEntry(outputVectorBPN, r, 0));
+
+	std::cout<<std::endl;
+}
 
 void Main::TestLightning()
 {
@@ -145,10 +189,11 @@ void Main::runCamera()
 
 		MooreBoundaryTracer::getContours(binaryImage, contourPoints, 100);
 		MooreBoundaryTracer::generateBoundaryImage(contourImage, contourPoints);
-		std::cout << "kekd" << std::endl;
-		//show16SImageClip(contourImage *255, "feed");
+		show16SImageClip(contourImage *255, "feed");
+		for(int contours = 0; contours < contourPoints.size(); contours++)
+			CheckContour(contourPoints[contours]);
 		//imshow("edges", binaryImage * 255);
-		if (cv::waitKey(30) >= 0) break;
+		if (cv::waitKey(1000) >= 0) break;
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
 }
@@ -253,7 +298,7 @@ void Main::trainNetwork()
 
 void Main::setTestData(cv::String name, cv::Mat classification)
 {
-	for(int imageIndex = 0; imageIndex < 6; imageIndex++)
+	for(int imageIndex = 0; imageIndex < 9; imageIndex++)
 	{
 		cv::Mat image;
 
@@ -301,7 +346,7 @@ void Main::setTestData(cv::String name, cv::Mat classification)
 
 		std::cout << name << "_" << imageIndex << "values" << std::endl;
 		std::cout << "circularity: " << circularity << std::endl;
-		std::cout << "isCOnvex: " << isConvex << std::endl;
+	//	std::cout << "isCOnvex: " << isConvex << std::endl;
 		std::cout << "convex defects: " << (double)defectPoints.size() / 100 << std::endl;
 		std::cout << "solidity: " << solidity << std::endl;
 		
--- Fase3/Fase3/Main.h
+++ Fase3/Fase3/Main.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <opencv2/core/cvstd.hpp>
 #include <opencv2/core/base.hpp>
+#include "BendingEnergy.h"
 
 class Main
 {
@@ -8,6 +9,7 @@ public:
 	Main();
 	~Main();
 	void TestHeart();
+	void CheckContour(std::vector<cv::Point> contourPoints);
 	void TestLightning();
 	void runCamera();
 	void createEmptyBorder(cv::Mat& image, int borderSize);
